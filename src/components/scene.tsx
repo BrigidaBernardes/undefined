7d1872723a679fb1231f3552c2989529c480f4d1
Bri <brigbernardes@gmail.com>
2024-06-12 09:35:29 -0300

    playing with drei and leva libs

--- src/components/scene.tsx
+++ src/components/scene.tsx
@@ -1,9 +1,14 @@
 "use client";
 import React, { useEffect, useRef } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { Environment, OrbitControls, Text } from "@react-three/drei";
+import {
+  Environment,
+  MeshTransmissionMaterial,
+  Text,
+} from "@react-three/drei";
 import { useMotionValue, useSpring } from "framer-motion";
 import { motion } from "framer-motion-3d";
+import { useControls } from "leva";
 
 function AnimatedMesh() {
   const mesh = useRef(null);
@@ -11,7 +16,6 @@ function AnimatedMesh() {
   const options = {
     damping: 20,
   };
-
 
   const mouse = {
     x: useSpring(useMotionValue(0), options),
@@ -21,8 +25,8 @@ function AnimatedMesh() {
   const manageMouseMove = (event: MouseEvent) => {
     const { innerWidth, innerHeight } = window;
     const { clientX, clientY } = event;
-    const targetX = -0.5 + (clientX / innerWidth);
-    const targetY = -0.5 + (clientY / innerHeight);
+    const targetX = -0.5 + clientX / innerWidth;
+    const targetY = -0.5 + clientY / innerHeight;
     mouse.x.set(targetX);
     mouse.y.set(targetY);
   };
@@ -34,21 +38,31 @@ function AnimatedMesh() {
   }, []);
 
   useFrame(() => {
-    mesh.current.rotation.x += 0.001;
-    mesh.current.rotation.y += 0.001;
+    mesh.current.rotation.x += 0.005;
+    mesh.current.rotation.y += 0.005;
   });
 
-  const {viewport} = useThree();
+  const { viewport } = useThree();
+  const materialProps = useControls({
+    thickness: { value: 0.25, min: 0, max: 3, step: 0.05 },
+    roughness: { value: 0.15, min: 0, max: 1, step: 0.1 },
+    transmission: { value: 1, min: 0, max: 1, step: 0.1 },
+    ior: { value: 0.9, min: 0, max: 3, step: 0.1 },
+    chromaticAberration: { value: 0.32, min: 0, max: 1 },
+    backside: { value: true },
+  });
 
   return (
-    <group scale={viewport.width / 8.5}>
-      <Text fontSize={1.2} fontWeight={700}>
-        hello world
+    <group>
+      <Text fontSize={1.2} fontWeight={700} font="fonts/ClimateCrisis.ttf">
+          Inovação  
       </Text>
-      <motion.mesh ref={mesh} rotation-x={mouse.y} rotation-y={mouse.x}>
-        <torusGeometry />
-        <meshStandardMaterial color={"hotpink"} />
-      </motion.mesh>
+      <group scale={viewport.width / 8.5}>
+        <motion.mesh ref={mesh} rotation-x={mouse.y} rotation-y={mouse.x}>
+          <torusGeometry />
+          <MeshTransmissionMaterial {...materialProps} />
+        </motion.mesh>
+      </group>
     </group>
   );
 }
@@ -56,10 +70,9 @@ function AnimatedMesh() {
 export default function Scene() {
   return (
     <Canvas>
-      <OrbitControls />
-      <directionalLight position={[0, 3, 2]} intensity={2.5} />
-      <Environment preset="sunset" />
-      <AnimatedMesh />
+        <directionalLight position={[0, 3, 2]} intensity={2.5} />
+        <Environment preset="city" />
+        <AnimatedMesh />
     </Canvas>
   );
 }
